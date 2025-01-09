68b0f215236006118f415437b015317d41afae1c
Yeimss <jamesherrera364@gmail.com>
2025-01-08 23:11:06 -0500

    Se realizan los dos retos iniciales respecto al operador Where

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
 ﻿LinqQueries quieries = new LinqQueries();
 
-ImprimirValores(quieries.GetAll());
+//Toda la colección
+//ImprimirValores(quieries.GetAll()); 
 
+//Libros con fecha de publicación >= 2000
+//ImprimirValores(quieries.LibrosPost2000()); 
+
+//250 paginas o mas y que el titulo contenga "in Action"
+ImprimirValores(quieries.LibrosInAction250Pages());
 void ImprimirValores(IEnumerable<Book> libros)
 {
-    Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+    Console.WriteLine("{0, -10} {1,-60} {2, 15} {3, 15}\n", "#", "Titulo", "N. Paginas", "Fecha publicacion");
+    int i = 1;
     foreach (var libro in libros)
     {
-        Console.WriteLine("{0,-60} {1, 15} {2, 15}", libro.Title, libro.PageCount, libro.PublishedDate.ToShortDateString());
+        Console.WriteLine("{0, -10} {1,-60} {2, 15} {3, 15}", i, libro.Title, libro.PageCount, libro.PublishedDate.ToShortDateString());
+        i++;
     }
 }
--- Utils/LinqQueries.cs
+++ Utils/LinqQueries.cs
@@ -15,4 +15,20 @@ public class LinqQueries
     {
         return librosCollection;
     }
+    public IEnumerable<Book> LibrosPost2000()
+    {
+        //Extension Method
+        //return librosCollection.Where(p => p.PublishedDate.Year > 2000); 
+
+        //Query Expresion
+        return from libros in librosCollection where libros.PublishedDate.Year > 2000 select libros;
+    }
+    public IEnumerable<Book> LibrosInAction250Pages()
+    {
+        //Extension Method
+        //return librosCollection.Where(p => p.PageCount > 250 && p.Title.Contains("in Action"));
+
+        //Query expresion
+        return from libros in librosCollection where libros.PageCount > 250 && libros.Title.Contains("in Action") select libros;
+    }
 }
