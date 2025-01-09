2d945749f8dd43f930264608d95839739a7b32df
Yeimss <jamesherrera364@gmail.com>
2025-01-08 22:49:39 -0500

    Se lee el archivo books.json

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿LinqQueries quieries = new LinqQueries();
+
+ImprimirValores(quieries.GetAll());
+
+void ImprimirValores(IEnumerable<Book> libros)
+{
+    Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+    foreach (var libro in libros)
+    {
+        Console.WriteLine("{0,-60} {1, 15} {2, 15}", libro.Title, libro.PageCount, libro.PublishedDate.ToShortDateString());
+    }
+}
--- Utils/LinqQueries.cs
+++ Utils/LinqQueries.cs
@@ -1,4 +1,18 @@
 public class LinqQueries
 {
+    private List<Book> librosCollection = new List<Book>();
 
+    public LinqQueries()
+    {
+        using (StreamReader reader = new StreamReader("books.json"))
+        {
+            string json = reader.ReadToEnd();
+            this.librosCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        }
+    }
+
+    public IEnumerable<Book> GetAll()
+    {
+        return librosCollection;
+    }
 }
