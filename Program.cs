13cffb818e98d3a33059ee39fa0c73a9a95689d3
Yeimss <jamesherrera364@gmail.com>
2025-01-12 11:31:17 -0500

    Se implementan operadores cómo All, Any y order by

--- Program.cs
+++ Program.cs
@@ -7,7 +7,22 @@
 //ImprimirValores(quieries.LibrosPost2000()); 
 
 //250 paginas o mas y que el titulo contenga "in Action"
-ImprimirValores(quieries.LibrosInAction250Pages());
+//ImprimirValores(quieries.LibrosInAction250Pages());
+
+//Todos los libros tienen status
+//Console.WriteLine($"¿Todos los libros tienen status?: {quieries.NingunLibroVacio()}");
+
+//Algun libro publicado en 2005
+//Console.WriteLine($"¿Algun libro publicado en 2005?: {quieries.HayLibro2005()}");
+
+//Libros python
+//ImprimirValores(quieries.LibrosPython());
+
+//Libros Java ordenados por titulo
+//ImprimirValores(quieries.LibrosCategoriaJava());
+
+//Libros Java ordenados por titulo
+ImprimirValores(quieries.Libros450PagDescendentes());
 void ImprimirValores(IEnumerable<Book> libros)
 {
     Console.WriteLine("{0, -10} {1,-60} {2, 15} {3, 15}\n", "#", "Titulo", "N. Paginas", "Fecha publicacion");
--- Utils/LinqQueries.cs
+++ Utils/LinqQueries.cs
@@ -20,7 +20,7 @@ public class LinqQueries
         //Extension Method
         //return librosCollection.Where(p => p.PublishedDate.Year > 2000); 
 
-        //Query Expresion
+        //Query Expression
         return from libros in librosCollection where libros.PublishedDate.Year > 2000 select libros;
     }
     public IEnumerable<Book> LibrosInAction250Pages()
@@ -28,7 +28,27 @@ public class LinqQueries
         //Extension Method
         //return librosCollection.Where(p => p.PageCount > 250 && p.Title.Contains("in Action"));
 
-        //Query expresion
+        //Query expression
         return from libros in librosCollection where libros.PageCount > 250 && libros.Title.Contains("in Action") select libros;
+    } 
+    public bool NingunLibroVacio()
+    {
+        return librosCollection.All(r => r.Status != string.Empty);
+    }
+    public bool HayLibro2005()
+    {
+        return librosCollection.Any(l => l.PublishedDate.Year == 2005);
+    }
+    public IEnumerable<Book> LibrosPython()
+    {
+        return librosCollection.Where(l => l.Title.ToLower().Contains("python"));
+    }
+    public IEnumerable<Book> LibrosCategoriaJava()
+    {
+        return librosCollection.Where(l => l.Categories.Contains("Java")).OrderBy(l => l.Title);
+    }
+    public IEnumerable<Book> Libros450PagDescendentes()
+    {
+        return librosCollection.Where(l => l.PageCount > 450).OrderByDescending(l => l.PageCount);
     }
 }
