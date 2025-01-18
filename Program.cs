9deb4a3470ddf10a7e31f857b40d7f684456d183
Yeimss <jamesherrera364@gmail.com>
2025-01-18 14:38:53 -0500

    Se aplica  Sum, Aggregate, AVG, Join, Group by, ToLookUp y Join

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿LinqQueries queries = new LinqQueries();
+﻿using System.Collections.Generic;
+
+LinqQueries queries = new LinqQueries();
 
 //Toda la colección
 //ImprimirValores(queries.GetAll()); 
@@ -34,7 +36,40 @@
 //ImprimirValores(queries.TresPrimersLibros());
 
 //Cantidad de libros entre 200 y 500 pag
-Console.WriteLine($"Cantidad de libros que tienen entre 200 y 500 páginas: {queries.CantidadEntre200Y500Pag()}");
+//Console.WriteLine($"Cantidad de libros que tienen entre 200 y 500 páginas: {queries.CantidadEntre200Y500Pag()}");
+
+//Menor fecha de publicación
+//Console.WriteLine($"Menor fecha de publicación: {queries.MinimaFechaDePublicacion()}");
+
+//Mayor número de páginas
+//Console.WriteLine($"Mayor número de páginas: {queries.MaximoNumeroDePaginas()}");
+
+//Minimo numero de paginas diferente de cero
+//Book libro = queries.MenorNumeroDePaginasMayorQueCero();    
+//Console.WriteLine($"El libro con menor número de páginas es {libro.Title} con {libro.PageCount} páginas");
+
+//Libro con fecha de publicación mayor
+//libro = queries.LibroConFechaMasReciente();
+//Console.WriteLine($"El libro con la fecha más reciente es {libro.Title} y fue publicado el {libro.PublishedDate.ToShortDateString()}");
+
+//Suma total de paginas de todos los libros entre 0 y 500
+//Console.WriteLine($"Suma de paginas de libros entre 0 y 500 paginas: {queries.CantPagBetween0and500()}");
+
+//Titulos de los libros que son mayores al 2015
+//Console.WriteLine($"Libros publicados después del 2015: \n{queries.TituloLibrosDespuesDel2015Concatenados()}");
+
+//Promedio de caracteres que tienen los titulos de la colección
+//Console.WriteLine($"Promedio de caracteres por titulo: {queries.promedioCaracteresTitulos()}");
+
+//Libros después del 2000 agrupados por año
+//ImprimirGrupo(queries.librosPublicadosDespuesDel2000AgrupadosPorAno());
+
+//LookUp con la inicial del libro
+//var dictionaryLoop = queries.DiccionariosDeLibrosPorLetra();
+//ImprimirDiccionario(dictionaryLoop, 'S');
+
+//Join
+ImprimirValores(queries.Join500PagAndPost2005());
 void ImprimirValores(IEnumerable<Book> libros)
 {
     Console.WriteLine("{0, -10} {1,-60} {2, 15} {3, 15}\n", "#", "Titulo", "N. Paginas", "Fecha publicacion");
@@ -43,5 +78,26 @@ void ImprimirValores(IEnumerable<Book> libros)
     {
         Console.WriteLine("{0, -10} {1,-60} {2, 15} {3, 15}", i, libro.Title, libro.PageCount, libro.PublishedDate.ToShortDateString());
         i++;
+    }
+}
+void ImprimirGrupo(IEnumerable<IGrouping<int, Book>> ListadeLibros)
+{
+    foreach (var grupo in ListadeLibros)
+    {
+        Console.WriteLine("");
+        Console.WriteLine($"Grupo: {grupo.Key}");
+        Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+        foreach (var item in grupo)
+        {
+            Console.WriteLine("{0,-60} {1, 15} {2, 15}", item.Title, item.PageCount, item.PublishedDate.Date.ToShortDateString());
+        }
+    } 
+}
+void ImprimirDiccionario(ILookup<char, Book> ListadeLibros, char letra)
+{
+    Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+    foreach (var item in ListadeLibros[letra])
+    {
+        Console.WriteLine("{0,-60} {1, 15} {2, 15}", item.Title, item.PageCount, item.PublishedDate.Date.ToShortDateString());
     }
 }
--- Utils/LinqQueries.cs
+++ Utils/LinqQueries.cs
@@ -30,7 +30,7 @@ public class LinqQueries
 
         //Query expression
         return from libros in librosCollection where libros.PageCount > 250 && libros.Title.Contains("in Action") select libros;
-    } 
+    }
     public bool NingunLibroVacio()
     {
         return librosCollection.All(r => r.Status != string.Empty);
@@ -70,5 +70,61 @@ public class LinqQueries
     public int CantidadEntre200Y500Pag()
     {
         return librosCollection.Count(l => l.PageCount >= 200 && l.PageCount <= 500);
+    }
+    public DateTime MinimaFechaDePublicacion()
+    {
+        return librosCollection.Min(l => l.PublishedDate);
+    }
+    public int MaximoNumeroDePaginas()
+    {
+        return librosCollection.Max(l => l.PageCount);
+    }
+    public Book MenorNumeroDePaginasMayorQueCero()
+    {
+        return librosCollection.Where(l=>l.PageCount != 0).MinBy(l => l.PageCount);
+    }
+    public Book LibroConFechaMasReciente()
+    {
+        return librosCollection.MaxBy(l => l.PublishedDate);
+    }
+    public int CantPagBetween0and500()
+    {
+        return librosCollection.Where(l => l.PageCount <= 500).Sum(l => l.PageCount);
+    }
+    public string TituloLibrosDespuesDel2015Concatenados()
+    {
+        return librosCollection.Where(l => l.PublishedDate.Year >= 2015)
+            .Aggregate("", (titulosLibros, next) =>
+            {
+                titulosLibros += $" - {next.Title}\n";
+                return titulosLibros;
+            });
+            
+    }
+    public double promedioCaracteresTitulos()
+    {
+        return librosCollection.Average(l => l.Title.Length);
+    }
+    public IEnumerable<IGrouping<int, Book>> librosPublicadosDespuesDel2000AgrupadosPorAno()
+    {
+        return librosCollection.Where(l => l.PublishedDate.Year >= 2000).GroupBy(l => l.PublishedDate.Year);
+    }
+    public ILookup<char, Book> DiccionariosDeLibrosPorLetra()
+    {
+        return librosCollection.ToLookup(l => l.Title[0], l => l );
+    }
+    public IEnumerable<Book> LibrosDespuesDel2005()
+    {
+        return librosCollection.Where(l => l.PublishedDate.Year >= 2005);
+    }
+    public IEnumerable<Book> LibrosSuperioresA500Pag()
+    {
+        return librosCollection.Where(l => l.PageCount >= 500);
+    }
+    public IEnumerable<Book> Join500PagAndPost2005()
+    {
+        IEnumerable<Book> post2005 = LibrosDespuesDel2005();
+        IEnumerable<Book> moreThan500Pag = LibrosSuperioresA500Pag();
+        return post2005.Join(moreThan500Pag, p => p.Title, x => x.Title, (p, x) => p);
     }
 }
